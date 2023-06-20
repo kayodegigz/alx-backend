d3c0141aa6f0f416ffcc659709e1a3fdbb0b7273
kayodegigz <kayogunrinde@gmail.com>
2023-06-20 22:16:22 +0100

    LRU cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" A caching system that removes the least recently used items """
+
+from collections import deque
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ Cache that implements LRU and extends BaseCache
+    """
+
+    def __init__(self):
+        """ this is the constructor """
+        super().__init__()
+        self.queue = deque()
+
+    def put(self, key, item):
+        """  saves an item in cache to the specified key
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            if len(self.cache_data) == self.MAX_ITEMS:
+                discarded = self.queue.popleft()
+                del self.cache_data[discarded]
+                print("DISCARD: {}".format(discarded))
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ retrieves the value in self.cache_data linked to key
+        """
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data.get(key)
